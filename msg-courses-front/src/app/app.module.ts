6a0609710a93ce1068e87df0b01dc56775526bd9
maresb <bianca.mares13@yahoo.com>
2017-12-04 14:33:58 +0200

    front app and courses component init

---
+++ msg-courses-front/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+
+import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses/courses.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    CoursesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
