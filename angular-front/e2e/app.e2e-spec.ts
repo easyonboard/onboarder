2f1f5a910d5961fc4b19f3c5fc062d457065ec0e
maresb <bianca.mares13@yahoo.com>
2017-12-04 15:03:33 +0200

    changed front name

--- angular-front/e2e/app.e2e-spec.ts
+++ angular-front/e2e/app.e2e-spec.ts
@@ -1,6 +1,6 @@
 import { AppPage } from './app.po';
 
-describe('msg-courses-front App', () => {
+describe('angular-front App', () => {
   let page: AppPage;
 
   beforeEach(() => {
--- angular-front/src/app/app.module.ts
+++ angular-front/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { CoursesComponent } from './courses/courses.component';
@@ -12,6 +12,7 @@ import { CoursesComponent } from './courses/courses.component';
     CoursesComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
