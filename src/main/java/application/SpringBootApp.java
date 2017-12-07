66f9fe2e0b182dc0b4ab17f298904bb3d01c8c8a
Iulia T <iulia.tica05@gmail.com>
2017-12-07 11:42:15 +0200

    Persistence module

--- src/main/java/application/SpringBootApp.java
+++ src/main/java/application/SpringBootApp.java
@@ -2,11 +2,15 @@ package application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
 
+@EntityScan(basePackages = {"entity"})
+//@ComponentScan(basePackages={"controller"})
 @SpringBootApplication
 public class SpringBootApp {
     public static void main(String[] args) {
-
         SpringApplication.run(SpringBootApp.class, args);
+
     }
 }
