20a3b693e365c9458e03bfbcd434965f9e829f4e
Iulia T <iulia.tica05@gmail.com>
2018-01-29 14:34:14 +0100

    new module for SpringBoot

--- app/src/main/java/SpringBootApp.java
+++ app/src/main/java/SpringBootApp.java
@@ -1,5 +1,3 @@
-package application;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
