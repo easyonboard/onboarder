2b3f91820c051e382ca7d974c877d87a971e4a10
ticai <iulia.tica05@gmail.com>
2018-02-13 17:27:50 +0200

    configure Docker for server

--- server/app/src/main/java/SpringBootApp.java
+++ server/app/src/main/java/SpringBootApp.java
@@ -1,10 +1,16 @@
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @EntityScan(basePackages = {"entity"})
-@ComponentScan(basePackages={"controller","dao","service", "validator", "utilityService"})
+@ComponentScan(basePackages = {"controller", "dao", "service", "validator", "utilityService"})
 @SpringBootApplication
 public class SpringBootApp {
     public static void main(String[] args) {
