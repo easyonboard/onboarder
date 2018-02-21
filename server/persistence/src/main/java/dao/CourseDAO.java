e7f9dd1016eaad7e4c340b2b50d6aa76f62b29ef
ticai <iulia.tica05@gmail.com>
2018-02-21 11:13:25 +0200

    Changes in CourseDAO at allCourses() method

--- server/persistence/src/main/java/dao/CourseDAO.java
+++ server/persistence/src/main/java/dao/CourseDAO.java
@@ -16,20 +16,19 @@ public class CourseDAO extends AbstractDAO<Course> {
     }
 
     /**
-     *
      * @return List of all courses
      */
     public List<Course> allCourses() {
         CriteriaBuilder cb = this.getCriteriaBuilder();
         CriteriaQuery<Course> criteriaQuery = cb.createQuery(Course.class);
-        Root<Course> rootUser = criteriaQuery.from(Course.class);
-        criteriaQuery.select(rootUser);
+        Root<Course> rootCourse = criteriaQuery.from(Course.class);
+        criteriaQuery.select(cb.construct(Course.class, rootCourse.get("idCourse"), rootCourse.get("titleCourse"),
+                rootCourse.get("overview")));
         return (List<Course>) this.executeCriteriaQuery(criteriaQuery);
     }
 
     /**
-     *
-     * @param  overview the course overview
+     * @param overview the course overview
      * @return List of Courses which contains the overview parameter
      */
     public List<Course> searchByOverview(String overview) {
@@ -37,7 +36,7 @@ public class CourseDAO extends AbstractDAO<Course> {
         CriteriaQuery<Course> criteriaQuery = cb.createQuery(Course.class);
         Root<Course> rootCourse = criteriaQuery.from(Course.class);
         criteriaQuery.select(cb.construct(Course.class,
-             rootCourse.get("idCourse"), rootCourse.get("titleCourse"), rootCourse.get("overview"))).where(cb.like(cb.upper(rootCourse.get("overview")), "%"+overview.toUpperCase()+"%"));
+                rootCourse.get("idCourse"), rootCourse.get("titleCourse"), rootCourse.get("overview"))).where(cb.like(cb.upper(rootCourse.get("overview")), "%" + overview.toUpperCase() + "%"));
         List<Course> courses = this.executeCriteriaQuery(criteriaQuery);
         return courses;
     }
