84fbc62b9bab162dd9a2eea5807b5a1cd465ae96
ticai <iulia.tica05@gmail.com>
2018-03-14 13:31:39 +0200

    add 'Add course' button only if user it\'s logged

--- client/angular-front/src/app/login/login.component.ts
+++ client/angular-front/src/app/login/login.component.ts
@@ -1,8 +1,8 @@
 import {AfterContentInit, Component, ElementRef, OnInit,} from '@angular/core';
-import {UserService} from "../service/user.service";
-import {UserDTO} from "../domain/user";
-import {RootConst} from "../util/RootConst";
-import {Router} from "@angular/router";
+import {UserService} from '../service/user.service';
+import {UserDTO} from '../domain/user';
+import {RootConst} from '../util/RootConst';
+import {Router} from '@angular/router';
 import index from '@angular/cli/lib/cli';
 
 @Component({
@@ -20,42 +20,42 @@ export class LoginComponent implements OnInit, AfterContentInit {
   public rootConst: RootConst;
   private currentComponentElement: HTMLElement;
   private userNotfound: string;
-  private headerDiv:NodeListOf<Element>;
-  private footerDiv:Element;
+  private headerDiv: NodeListOf<Element>;
+  private footerDiv: Element;
 
   constructor(private userService: UserService, private router: Router, private elemRef: ElementRef) {
     this.option = false;
   }
 
   ngOnInit() {
-    this.userNotfound = "";
-    this.message = "Welcome back!";
-    this.messageSignIn = "Sign Up for Free";
+    this.userNotfound = '';
+    this.message = 'Welcome back!';
+    this.messageSignIn = 'Sign Up for Free';
     this.rootConst = new RootConst();
 
   }
 
   ngAfterContentInit() {
     this.currentComponentElement = this.elemRef.nativeElement.previousElementSibling;
-    this.footerDiv = this.currentComponentElement.parentElement.getElementsByClassName("footerDiv").item(0)
+    this.footerDiv = this.currentComponentElement.parentElement.getElementsByClassName('footerDiv').item(0);
 
-    this.headerDiv= this.currentComponentElement.getElementsByClassName("headerDiv");
-    for(let index=0; index<this.headerDiv.length;index++){
-      (<HTMLElement>this.headerDiv.item(index)).style.visibility="hidden";
+    this.headerDiv = this.currentComponentElement.getElementsByClassName('headerDiv');
+    for (let index = 0; index < this.headerDiv.length; index++) {
+      (<HTMLElement>this.headerDiv.item(index)).style.visibility = 'hidden';
     }
 
     if (this.footerDiv != null) {
-      (<HTMLElement>this.footerDiv).style.visibility="hidden";
+      (<HTMLElement>this.footerDiv).style.visibility = 'hidden';
     }
   }
 
   createNewAccount(option: boolean): void {
     if (true === option) {
-      document.getElementById("signUp").style.backgroundColor = "#841439";
-      document.getElementById("login").style.background = "#d3d3d3";
+      document.getElementById('signUp').style.backgroundColor = '#841439';
+      document.getElementById('login').style.background = '#d3d3d3';
     } else {
-      document.getElementById("signUp").style.backgroundColor = "#d3d3d3";
-      document.getElementById("login").style.backgroundColor = "#841439";
+      document.getElementById('signUp').style.backgroundColor = '#d3d3d3';
+      document.getElementById('login').style.backgroundColor = '#841439';
     }
     this.option = option;
   }
@@ -65,22 +65,21 @@ export class LoginComponent implements OnInit, AfterContentInit {
     password = password.trim();
     this.userService.login({username, password} as UserDTO).subscribe(res => {
         if (res.username.length > 0) {
-          localStorage.setItem("userLogged", res.username);
-          localStorage.setItem("userLoggedId", res.idUser.toString());
-          console.log(this.headerDiv)
-          debugger
-          for(let index=0; index<this.headerDiv.length;index++){
-            (<HTMLElement>this.headerDiv.item(index)).style.visibility="visible";
+          localStorage.setItem('userLogged', res.username);
+          localStorage.setItem('userLoggedId', res.idUser.toString());
+          console.log(this.headerDiv);
+          for (let index = 0; index < this.headerDiv.length; index++) {
+            (<HTMLElement>this.headerDiv.item(index)).style.visibility = 'visible';
           }
           if (this.footerDiv != null) {
-            (<HTMLElement>this.footerDiv).style.visibility="visible";
+            (<HTMLElement>this.footerDiv).style.visibility = 'visible';
           }
           this.router.navigateByUrl(this.rootConst.FRONT_COURSES_PAGE);
         }
       },
       err => {
-        if (err.status == 403) {
-          this.userNotfound = "Username or password wrong! Please try again.";
+        if (err.status === 403) {
+          this.userNotfound = 'Username or password wrong! Please try again.';
         }
         console.log(err);
       });
@@ -93,15 +92,15 @@ export class LoginComponent implements OnInit, AfterContentInit {
     email = email.trim();
     password = password.trim();
     passwordII = passwordII.trim();
-    if (password != passwordII) {
-      this.errorMessage = "Password does not match";
+    if (password !== passwordII) {
+      this.errorMessage = 'Password does not match';
       return;
     }
-
     else {
       this.userService.addUser({name, username, email, password} as UserDTO).subscribe(
         res => {
-          this.option = false;        },
+          this.option = false;
+        },
         err => {
           this.errorMessage = err.error.message;
         });
