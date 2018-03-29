1573a6231091d73f65a35db2bab5e1d9f494a333
INT\cotete <eusebio.cotet@msg.group>
2018-03-29 15:20:32 +0300

    refactored User_Information table, resolved conflicts

--- server/persistence/src/main/java/entity/User.java
+++ server/persistence/src/main/java/entity/User.java
@@ -37,10 +37,6 @@ public class User implements Serializable {
     @NotNull
     private String email;
 
-    @OneToOne
-    @JoinColumn(name = "idUserInfo")
-    private UserInfo userInfo;
-
     @ManyToOne
     private Role role;
 
