bf5ba81bf016a7d217b0839a6ad9b4f509e3e182
CorinaAlb <cori_alb@yahoo.com>
2018-04-16 12:08:51 +0300

    project field on frontend added

--- client/angular-front/src/app/users/user-info-update/user-info-update.component.ts
+++ client/angular-front/src/app/users/user-info-update/user-info-update.component.ts
@@ -24,6 +24,7 @@ export class UserInfoUpdateComponent implements OnInit {
     this.childUserInfoFormularComponent.userInformation.team = this.userInformation.team;
     this.childUserInfoFormularComponent.userInformation.building = this.userInformation.building;
     this.childUserInfoFormularComponent.userInformation.store = this.userInformation.store;
+    this.childUserInfoFormularComponent.userInformation.project = this.userInformation.project;
     this.childUserInfoFormularComponent.userInformation.department = this.userInformation.department;
     this.childUserInfoFormularComponent.userInformation.buddyUser = this.userInformation.buddyUser;
     this.childUserInfoFormularComponent.userInformation.startDate = this.userInformation.startDate;
