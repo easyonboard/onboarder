e09041d8f2cdead50e8f53bb26e9582e0d442aac
biancamares13 <biancamares13@yahoo.com>
2018-04-17 13:52:54 +0300

    close checklist on save and noresultexception fixed

--- server/persistence/src/main/java/dao/CheckListDAO.java
+++ server/persistence/src/main/java/dao/CheckListDAO.java
@@ -4,6 +4,7 @@ import entity.CheckList;
 import entity.User;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.NoResultException;
 import javax.persistence.Query;
 
 @Service
@@ -16,9 +17,13 @@ public class CheckListDAO extends AbstractDAO<CheckList> {
 
     public CheckList findByUser(User userEntity) {
 
-        Query query=em.createQuery("Select c from CheckList c where c.userAccount=:user");
+        Query query = em.createQuery("Select c from CheckList c where c.userAccount=:user");
         query.setParameter("user", userEntity);
-        return (CheckList) query.getSingleResult();
+        try {
+            return (CheckList) query.getSingleResult();
 
+        } catch (NoResultException e) {
+            return null;
+        }
     }
 }
