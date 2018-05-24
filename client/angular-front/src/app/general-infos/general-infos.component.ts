38ea37ad6e02254930a1f8f09309a80e4a95156c
biancamares13 <biancamares13@yahoo.com>
2018-05-24 10:40:27 +0300

    small changes

--- client/angular-front/src/app/general-infos/general-infos.component.ts
+++ client/angular-front/src/app/general-infos/general-infos.component.ts
@@ -20,7 +20,7 @@ export class GeneralInfosComponent implements OnInit {
     this.infos.set('https://team.msg.de/site/msg%20systems%20RO/SitePages/Homepage.aspx', 'Team site .msg Systems Romania');
     this.infos.set('https://guru.msg.de/', '.msg Systems Global');
     this.infos.set('https://team.msg.de/site/msg%20systems%20RO/Docs/Administrativ/Organigrama/Orga%2026.02.2018.png', '.msg Systems Romania department structure');
-    this.infos.set('', '.msg Gillardon Global department structure(de luat de la Miruna)');
+    // this.infos.set('', '.msg Gillardon Global department structure(de luat de la Miruna)');
     this.infos.set('https://team.msg.de/site/msg%20systems%20RO/SitePages/Persoane%20de%20contact.aspx', 'Contact persons (Administrativ department && HR)');
     this.infos.set('https://team.msg.de/site/msg%20systems%20RO/SitePages/Beneficiile%20tale.aspx', 'Your benefits in .msg');
     this.infos.set('https://team.msg.de/site/msg%20systems%20RO/SitePages/Concedii.aspx', 'Holidays');
