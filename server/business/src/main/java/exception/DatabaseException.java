d83ae5996b0079a79730b40958aee940b271a66d
Corina Alb <cori_alb@yahoo.com>
2018-07-25 09:26:29 +0300

    exceptions

---
+++ server/business/src/main/java/exception/DatabaseException.java
@@ -0,0 +1,12 @@
+package exception;
+
+/**
+ * Exception thrown
+ */
+public class DatabaseException extends Exception {
+
+    public DatabaseException(String message) {
+        super(message);
+    }
+
+}
--- server/business/src/main/java/exception/EntityNotFoundException.java
+++ server/business/src/main/java/exception/EntityNotFoundException.java
@@ -1,7 +1,7 @@
 package exception;
 
 /**
- * Exception thrown when a user entity is not found in the database
+ * Exception thrown
  */
 public class EntityNotFoundException extends Exception {
 
