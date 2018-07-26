548cb1116b5f662c5494b1f5f34c124a8789b7ee
INT\cotete <eusebio.cotet@msg.group>
2018-07-26 13:34:48 +0300

    modified methods, added exceptions where it is possible

--- server/business/src/main/java/service/CheckListService.java
+++ server/business/src/main/java/service/CheckListService.java
@@ -1,12 +1,13 @@
 package service;
 
-import dao.CheckListDAO;
 import dao.CheckListRepository;
 import dto.CheckListDTO;
 import dto.UserInformationDTO;
 import dto.mapper.CheckListMapper;
 import entity.CheckList;
 import entity.User;
+import exception.types.EntityNotFoundException;
+import exception.types.InvalidDataException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -18,21 +19,28 @@ public class CheckListService {
 
     private CheckListMapper checkListMapper = CheckListMapper.INSTANCE;
 
-    public CheckListDTO addCheckList(UserInformationDTO userInformationDTO, User appUser) {
+    public CheckListDTO addCheckList(UserInformationDTO userInformationDTO, User appUser) throws InvalidDataException {
 
+        if (userInformationDTO == null || appUser == null){
+            throw new InvalidDataException("Checklist data is invalid");
+        }
         CheckList checkList = new CheckList();
-        checkList.setHasBuddyAssigned(userInformationDTO.getBuddyUser()!=null);
+        checkList.setHasBuddyAssigned(userInformationDTO.getBuddyUser() != null);
         checkList.setUserAccount(appUser);
         checkListRepository.save(checkList);
 
         return checkListMapper.mapToDTO(checkListRepository.save(checkList));
     }
 
-    public CheckListDTO findByUser(User userEntity) {
-        return checkListMapper.mapToDTO(checkListRepository.findByUserAccount(userEntity));
+    public CheckListDTO findByUser(User userEntity) throws  EntityNotFoundException {
+        CheckList checkList = checkListRepository.findByUserAccount(userEntity);
+        if (checkList == null){
+            throw new EntityNotFoundException("checklist could not be found in the database");
+        }
+        return checkListMapper.mapToDTO(checkList);
     }
 
-    public boolean isMailSentToUser(User userEntity) {
+    public boolean isMailSentToUser(User userEntity) throws  EntityNotFoundException{
         return this.findByUser(userEntity).isMailSent();
     }
 
--- server/persistence/src/main/java/dao/CheckListDAO.java
+++ server/persistence/src/main/java/dao/CheckListDAO.java
@@ -1,42 +1,33 @@
 package dao;
 
 import entity.CheckList;
-import entity.User;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
-import java.lang.reflect.Field;
-
 @Service
-public class CheckListDAO extends AbstractDAO<CheckList>  {
-
-    @Autowired
-    private CheckListRepository checkListRepository;
+public class CheckListDAO extends AbstractDAO<CheckList> {
 
     @Override
     public Class<CheckList> getEntityClass() {
-
         return CheckList.class;
     }
 
-
-    @Transactional
-    public void setValue(User user, String fieldName, boolean value) {
-
-        try {
-            CheckList checkListForUser = checkListRepository.findByUserAccount(user);
-            Field field = null;
-            field = CheckList.class.getDeclaredField(fieldName);
-
-            field.setAccessible(true);
-            field.set(checkListForUser, value);
-
-            this.persistEntity(checkListForUser);
-        } catch (NoSuchFieldException e) {
-            e.printStackTrace();
-        } catch (IllegalAccessException e) {
-            e.printStackTrace();
-        }
-    }
+//    eusebio --- found no usage for method, commented it.
+//    @Transactional
+//    public void setValue(User user, String fieldName, boolean value) {
+//
+//        try {
+//            CheckList checkListForUser = checkListRepository.findByUserAccount(user);
+//            Field field = null;
+//            field = CheckList.class.getDeclaredField(fieldName);
+//
+//            field.setAccessible(true);
+//            field.set(checkListForUser, value);
+//
+//            this.persistEntity(checkListForUser);
+//        } catch (NoSuchFieldException e) {
+//            e.printStackTrace();
+//        } catch (IllegalAccessException e) {
+//            e.printStackTrace();
+//        }
+//    }
 }
