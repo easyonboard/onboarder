d7cbc6780dc58b3a826887a68873452a5afa947a
ticai <iulia.tica05@gmail.com>
2018-07-31 09:25:36 +0300

    good merge

---
+++ server/business/src/main/java/dto/ContactPersonDTO.java
@@ -0,0 +1,27 @@
+package dto;
+
+public class ContactPersonDTO {
+
+    private String username;
+    private String msgMail;
+
+    public String getUsername() {
+
+        return username;
+    }
+
+    public void setUsername(String username) {
+
+        this.username = username;
+    }
+
+    public String getMsgEmail() {
+
+        return msgMail;
+    }
+
+    public void setMsgMail(String msgMail) {
+
+        this.msgMail = msgMail;
+    }
+}
