2945cb27658ae033b9841d10cb76df6b1d729408
biancamares13 <biancamares13@yahoo.com>
2018-10-08 16:17:28 +0300

    sql scripts

---
+++ documentation/eob_public_department.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.department (id_department, department_name, parent_id_department) VALUES (1, 'GENERAL', null);
+INSERT INTO public.department (id_department, department_name, parent_id_department) VALUES (2, 'GILLARDON', 1);
+INSERT INTO public.department (id_department, department_name, parent_id_department) VALUES (3, 'INSSURANCE', 1);
+INSERT INTO public.department (id_department, department_name, parent_id_department) VALUES (4, 'AUTOMOTIVE', 1);
+INSERT INTO public.department (id_department, department_name, parent_id_department) VALUES (5, 'PROJEKTE', 2);
+INSERT INTO public.department (id_department, department_name, parent_id_department) VALUES (6, 'PRODUKTE', 2);
---
+++ documentation/eob_public_meeting_hall.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3000, 8, 11, 'Moon I', 2001);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3001, 5, 11, 'Moon II', 2001);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3002, 8, 1, 'Mars-1', 2000);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3003, 10, 3, 'Mercury-3', 2000);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3004, 11, 4, 'Uranus-3', 2000);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3005, 2, 0, 'Moldoveanu', 2002);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3006, 25, 0, 'Hymalaya', 2002);
+INSERT INTO public.meeting_hall (id_meeting_hall, capacity, floor, hall_name, location_id_location) VALUES (3007, 15, 0, 'Fuji', 2002);
